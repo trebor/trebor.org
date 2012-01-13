1e58401af63bd1801dd4b1ae735366fe1e649266
Robert Harris <github.com@trebor.org>
2012-01-12 17:03:24 -0800

    added more content.

--- src/main/java/org/trebor/www/store/MarkupRenderer.java
+++ src/main/java/org/trebor/www/store/MarkupRenderer.java
@@ -5,7 +5,7 @@ import info.bliki.wiki.model.WikiModel;
 
 public class MarkupRenderer extends WikiModel
 {
-  public static final String BASE = "/trebor/";
+  public static final String BASE = "/";
   public static final String IMAGE_BASE = BASE + "static/assets/images/";
   public static final String PAGE_BASE = BASE + "static/treemenu.html?page=";
   private static Configuration mConfiguration = new Configuration()
@@ -13,6 +13,7 @@ public class MarkupRenderer extends WikiModel
     {
       addInterwikiLink("wikipedia", "http://wikipedia.org/wiki/${title}");
       addInterwikiLink("youtube", "http://www.youtube.com/watch?v=${title}");
+      addInterwikiLink("trebor-static", BASE + "static/${title}");
     }
   };
 
