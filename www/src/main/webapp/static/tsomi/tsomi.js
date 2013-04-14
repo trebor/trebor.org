769f3fb376ab77da771a1618f97754dfddfbad55
stf <stillsmall@gmail.com>
2013-04-14 15:41:11 -0700

    fixed title

--- www/src/main/webapp/static/tsomi/tsomi.js
+++ www/src/main/webapp/static/tsomi/tsomi.js
@@ -167,13 +167,13 @@ defs.append("path")
 
 // add title
 
-svg.append("text")
-  .classed("title", true)
-  .classed("static-text", true)
-  .attr("dx", "120")
-  .append("textPath")
-  .attr("xlink:href", "#titlepath")
-  .text("The Sphere Of My Influence");
+// svg.append("text")
+//   .classed("title", true)
+//   .classed("static-text", true)
+//   .attr("dx", "120")
+//   .append("textPath")
+//   .attr("xlink:href", "#titlepath")
+//   .text("The Sphere Of My Influence");
 
 svg.append("text")
   .classed("about", true)
